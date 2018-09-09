4ad3cc92b090f9ec380c85278370ff3ca11e5860
Prince <pm11prince@gmail.com>
2018-09-09 17:41:42 +0530

    updated file

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var router = require('./lib/routers/calcRouter');
 app.use("/calculator", router);
-app.listen(5000);
+app.listen(8080);
 exports.app = app;
 exports.request = request;
 
